c80a9ec9129c0efcf2be90e560cdb1bcdd9ee9af
arnavanand2005 <arnavanand2005@gmail.com>
2025-08-14 21:00:36 +0530

    tailwind minor bug fixed

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const App = () => {
   return (
     <div className='text-bold text-2xl text-center mt-10'>
-      Hello World
+      <p>Hello World</p>
     </div>
   )
 }
