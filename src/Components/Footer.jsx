c2cd4f210dcc8e6c09cdcf5a146baf7094573d3e
arnavanand2005 <arnavanand2005@gmail.com>
2025-08-15 22:49:39 +0530

    components added

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
+import { FaGithub } from "react-icons/fa";
+import { CiLinkedin } from "react-icons/ci";
 
 const Footer = () => {
   return (
     <div>
-      This is the footer
+        <a href="https://github.com/arnavanand2005"><FaGithub /></a>
+        <a href="https://www.linkedin.com/in/arnav-anand-056710315"><CiLinkedin /></a>
     </div>
   )
 }
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
 
+
 const Navbar = () => {
   return (
-    <div>
-      This is Navbar
+    <div className='background-navbar bg-black'>
+        <div className='text-white text-2xl font-bold p-4'>
+            ArnAv AnAnd
+        </div>
+        
     </div>
   )
 }
