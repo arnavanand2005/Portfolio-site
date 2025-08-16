06357d58687205dfd98ed835130dff8a1119254d
arnavanand2005 <arnavanand2005@gmail.com>
2025-08-16 23:45:25 +0530

    minor updates

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -5,13 +5,13 @@ import { IoIosMail , IoIosCall} from "react-icons/io";
 
 const Footer = () => {
   return (
-    <div className='flex flex-col justify-center items-center gap-4 bg-black text-white p-4 mt-4'>
+    <div className='flex flex-col justify-center p-6 md:p-8 items-center gap-6 bg-gradient-to-r from-gray-800 via-black to-gray-800 text-white mt-4 text-xl'>
       <div> 
       <p>2025 &copy; All rights reserved.</p>
       </div>
-      <div>
-        <p><IoIosMail/> : arnavanand2005@gmail.com</p>
-        <p><IoIosCall/> : +91-9117513766</p>
+      <div className='flex items-center gap-2'>
+        <p className='flex items-center gap-2 pr-3 '><IoIosMail/> : arnavanand2005@gmail.com</p>
+        <p className='flex items-center gp-2'><IoIosCall/> : +91-9117513766</p>
       </div>
       <div className='flex justify-center items-center gap-4 text-2xl'>
         <a className='text-grey-200' href="https://github.com/arnavanand2005"><FaGithub /></a>
