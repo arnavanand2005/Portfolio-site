a68eca3c15462e7d25387fe53bdf79a8d043d785
arnavanand2005 <arnavanand2005@gmail.com>
2025-09-06 16:01:05 +0530

    scroll

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,15 @@ import Home from './Pages/Home.jsx'
 import About from './Pages/About.jsx'
 import Contact from './Pages/Contact.jsx'
 import Projects from './Pages/Projects.jsx'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Routes} from 'react-router-dom'
+import ScrollTop from './Components/ScrollTop.jsx'
 
 const App = () => {
   return (
     <div>
       <Router>
         <Navbar />
+        <ScrollTop />
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/about" element={<About />} />
